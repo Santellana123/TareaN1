9748dae2f004b50cea9ec1d707caaa305b150a99
juan <juan@DESKTOP-UMCEALU>
2023-02-15 18:50:16 -0600

    Agregar archivos de proyecto.

---
+++ Tarrrea/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.NUMERO1 = New System.Windows.Forms.TextBox()
+        Me.NUMERO2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(325, 260)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "click"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(303, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(140, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "INTRODUCE TU NOMBRE"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(261, 157)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(207, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "INTRODUCE TU NUERO DE TELEFONO"
+        '
+        'NUMERO1
+        '
+        Me.NUMERO1.Location = New System.Drawing.Point(315, 92)
+        Me.NUMERO1.Name = "NUMERO1"
+        Me.NUMERO1.Size = New System.Drawing.Size(100, 23)
+        Me.NUMERO1.TabIndex = 3
+        '
+        'NUMERO2
+        '
+        Me.NUMERO2.Location = New System.Drawing.Point(315, 184)
+        Me.NUMERO2.Name = "NUMERO2"
+        Me.NUMERO2.Size = New System.Drawing.Size(100, 23)
+        Me.NUMERO2.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.NUMERO2)
+        Me.Controls.Add(Me.NUMERO1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents NUMERO1 As TextBox
+    Friend WithEvents NUMERO2 As TextBox
+End Class
---
+++ Tarrrea/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As String = NUMERO1.Text
+        Dim b As String = NUMERO2.Text
+        Dim fechaNacimiento As DateTime
+        fechaNacimiento = DateTime.Parse("2003/01/01")
+        Dim edad As String = (DateTime.Now.Year - fechaNacimiento.Year)
+        MsgBox(a + " " + b + " " + edad)
+
+    End Sub
+End Class
